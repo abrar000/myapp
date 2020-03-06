5783ffe998704162899b2cd30e0af21772fa43d2
abrar000 <wazirabrar@gmail.com>
2020-03-06 17:48:33 +0500

    Updates

---
+++ precache-manifest.c46d1bd7289625d917fa7a1d4e51f4bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea80fbaba03f47065e77ae8f6ff5f6be",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "b2e836b661bcb03b0051",
+    "url": "/myapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7790d732461058420aad",
+    "url": "/myapp/static/css/main.281a20d3.chunk.css"
+  },
+  {
+    "revision": "b2e836b661bcb03b0051",
+    "url": "/myapp/static/js/2.7d513983.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/myapp/static/js/2.7d513983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7790d732461058420aad",
+    "url": "/myapp/static/js/main.e51d0bbb.chunk.js"
+  },
+  {
+    "revision": "c1c547e046fb1a7e247b",
+    "url": "/myapp/static/js/runtime-main.7cc1b650.js"
+  }
+]);
